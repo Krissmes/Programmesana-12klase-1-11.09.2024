69e5c25a67982e9b59ce3997f49ae1a68e82ae8c
Krissmes <kriss.iesalnieks@gmail.com>
2024-09-13 14:17:54 +0300

    pabeidzu 1- 7 uzd
    done

--- skolas_uzdevumi_/uzdevums7.py
+++ skolas_uzdevumi_/uzdevums7.py
@@ -1,5 +1,8 @@
-dalamais = int(input("Ievadi skaitli no 1-100 un es tev pateikšu ar ko viņš dalās."))
+dalamais = int(input("Ievadi skaitli no 1-100 un es tev pateikšu visus skaitļus ar ko viņš dalās."))
 dalitajs = 1
 while dalitajs != 101:
-    if dalamais % dalitajs:
-        print("then")
+    if dalamais % dalitajs == 0:
+        print(dalamais, "dalās ar", dalitajs)
+        dalitajs +=1
+    else:
+        dalitajs +=1
